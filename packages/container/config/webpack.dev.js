b96c11048999d7c361a81d9e7df20d28b6a7e19d
kartikpandit79 <kartik.pandit79@gmail.com>
2024-05-28 08:39:11 +0530

    created deploy script

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -34,3 +34,10 @@ module.exports = merge(commonConfig, devConfig)
 // 4127 7671 7927 7128
 // CVV / CVV2 770 Good thru 8 / 2023
 // Alexis Edwards
+
+//     - uses: shinyinc / action - aws - cli@v1.2
+//         - run: aws s3 sync dist s3://${{ secrets.AWS_S3_BUCKET_NAME }}/container/latest
+// env:
+// AWS_ACCESS_KEY_ID: ${ { secrets.AWS_ACCESS_KEY_ID } }
+// AWS_SECRET_ACCESS_KEY: ${ { secrets.AWS_SECRET_ACCESS_KEY } }
+// AWS_DEFAULT_REGION: ""
