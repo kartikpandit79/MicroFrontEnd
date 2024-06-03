db88378ac665408d66a302c240c5e5ce4c62e448
kartikpandit79 <kartik.pandit79@gmail.com>
2024-06-03 09:30:03 +0530

    randomized css in js

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,15 +2,22 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom"
 import MarketingApp from "./Components/MarketingApp";
 import Header from "./Components/Header";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core/styles";
 
+
+const generateClassName = createGenerateClassName({
+    productionPrefix: "co"
+})
 
 const App = () => {
     return (
         <BrowserRouter>
-            <div>
-                <Header />
-                <MarketingApp />
-            </div>
+            <StylesProvider generateClassName={generateClassName}>
+                <div>
+                    <Header />
+                    <MarketingApp />
+                </div>
+            </StylesProvider>
         </BrowserRouter>
     )
 }
